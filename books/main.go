2f6e1bf5b5088c88aead4ec6afa482083d586d5f
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-22 23:05:43 +0600

    array

--- books/main.go
+++ books/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/new", handler2)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func handler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world")
 }
