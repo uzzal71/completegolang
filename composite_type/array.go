220bc780fb8c4739c68ed4b7fa79522d40940a07
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-22 23:29:43 +0600

    array

--- composite_type/array.go
+++ composite_type/array.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var a [3]int
-	for _, v := range a {
-		fmt.Printf("%d \n", v)
+	var q [3]int = [3]int{1, 2, 3}
+	var r [2]int = [2]int{1, 2}
+
+	for i, v := range q {
+		fmt.Printf("%d: %d\n", i, v)
 	}
+	for i, v := range r {
+        fmt.Printf("%d: %d\n", i, v)
+    }
 }
