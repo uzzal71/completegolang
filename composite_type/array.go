6fd61638a9ebad863aa789906e820fdf168f3233
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-23 23:32:23 +0600

    array

--- composite_type/array.go
+++ composite_type/array.go
@@ -1,20 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	type Currency int
-
-	const (
-		USD Currency = iota
-		EUR
-		GBP
-		RMB
-	)
-
-	symbol := [...]string{USD: "$", EUR: "E", GBP: "G", RMB: "R"}
-
-	for key, value := range symbol {
-		fmt.Println(key, value)
-	}
+	a := [2]int{1, 2}
 }
