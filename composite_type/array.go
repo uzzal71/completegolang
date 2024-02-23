d088a228f4295b5298b348abfeef7e96bb7ad1c8
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-23 23:35:37 +0600

    array

--- composite_type/array.go
+++ composite_type/array.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	a := [2]int{1, 2}
+	b := [...]int{1, 2}
+	c := [2]int{1, 3}
+	fmt.Println(a == b, a == c, b == c)
+
+	d := [3]int{1, 2}
+	fmt.Println(a == d)
 }
