eb75eb882507631aa83d82f266c332544b7d111c
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-23 23:39:33 +0600

    array

--- composite_type/array.go
+++ composite_type/array.go
@@ -3,11 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	a := [2]int{1, 2}
-	b := [...]int{1, 2}
-	c := [2]int{1, 3}
-	fmt.Println(a == b, a == c, b == c)
-
-	d := [3]int{1, 2}
-	fmt.Println(a == d)
+	data := []string{"foo", "bar"}
+	fmt.Printf("%q\n", data)
 }
