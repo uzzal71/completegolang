ce9e8440ba22d7ed959d4523a20d3f8792fddb4f
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-24 22:44:45 +0600

    structs

--- struct/main.go
+++ struct/main.go
@@ -1,21 +1,11 @@
 package main
 
-import "fmt"
-
 type person struct {
 	firstName string
 	lastName  string
 }
 
-type product struct {
-	name string
-	slug string
-	price float64
-	stock int
-}
-
 
 func main() {
-	product := product{name: "test", slug: "test", price: 25.36, stock: 100}
-	fmt.Println(product.name, product.slug, product.price)
+	
 }
