eada7a5ebd1daf2ff7ff87b941de8ce2b6355407
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-26 00:02:53 +0600

    data structure

---
+++ data-structure/slice_operations.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	mySlice := []int{10, 20, 30, 40, 50}
+
+	// accessing slice
+	fmt.Println(mySlice[2])
+	mySlice[2] = 100
+	fmt.Println(mySlice[2])
+
+	// appending slice
+	mySlice = append(mySlice, 60, 70, 80)
+    fmt.Println(mySlice)
+
+    // iterating over slice
+    for i, v := range mySlice {
+        fmt.Printf("index: %d, value: %d\n", i, v)
+    }
+}
