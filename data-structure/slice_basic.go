d28acf589d11255cca905c9e1fea3fb6bd88eb5a
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-25 23:43:46 +0600

    data structure

--- data-structure/slice_basic.go
+++ data-structure/slice_basic.go
@@ -14,4 +14,33 @@ func main() {
 	// Create slices
 	var mySlice []int
 	fmt.Println(mySlice)
+	fmt.Println(len(mySlice))
+	fmt.Printf("%T\n", mySlice)
+
+	// slice from array
+	arr := [5]int{10, 20, 30, 40, 50}
+	newSlice := arr[1:4]
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Printf("%T\n", newSlice)
+
+	// slice initialization
+	// var mySlice2 = []int{10, 20, 30, 40, 50}
+	mySlice2 := []int{100, 200, 300, 400, 500}
+
+	fmt.Println(mySlice2)
+	fmt.Println(len(mySlice2))
+	fmt.Printf("%T\n", mySlice2)
+
+	// using make
+	mySlice3 := make([]int, 5)
+    fmt.Println(mySlice3)
+    fmt.Println(len(mySlice3))
+    fmt.Printf("%T\n", mySlice3)
+
+    // append
+    mySlice3 = append(mySlice3, 60, 70, 80)
+    fmt.Println(mySlice3)
+    fmt.Println(len(mySlice3))
+    fmt.Printf("%T\n", mySlice3)
 }
