89a7cda5a06099a4ca0e4089b0f2285fe20c8764
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-26 23:58:15 +0600

    Error handling

--- 8.Error-Handling/error_interface.go
+++ 8.Error-Handling/error_interface.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func divide(a, b int) int {
-	return a / b
+func divide(a, b int) (int, error) {
+	if b == 0 {
+        return 0, errors.New("division by zero")
+    }
+	return a / b, nil
 }
 
 func main() {
-	fmt.Println(divide(4, 0))
+	result, err := divide(4, 0)
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		return
+	}
+	fmt.Println(result)
 }
