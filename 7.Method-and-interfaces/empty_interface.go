fe21e4a310d9609ca8108ebfa58f0b0ef5f592bf
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-26 16:32:28 +0600

    pointers

---
+++ 7.Method-and-interfaces/empty_interface.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// type I interface {
+
+// }
+
+func PrintType(i interface{}) {
+	switch i.(type) {
+	case int:
+		fmt.Println("It's an integer")
+	case string:
+		fmt.Println("It's a string")
+	default:
+		fmt.Println("Different type")
+	}
+}
+
+func main() {
+	PrintType(5)
+	PrintType("Hello")
+	PrintType(1.2)
+}
