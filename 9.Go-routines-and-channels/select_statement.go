03f6f2539ffe2649bd8b61abebe1f74ad89113ea
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-27 23:27:17 +0600

    go routines and channels

--- 9.Go-routines-and-channels/select_statement.go
+++ 9.Go-routines-and-channels/select_statement.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"time"
+)
+
+func server1(ch1 chan string) {
+	time.Sleep(2 * time.Second)
+	ch1 <- "form server 1"
+}
+
+func server2(ch2 chan string) {
+	time.Sleep(2 * time.Second)
+	ch2 <- "form server 2"
+}
+
+func main() {
+	output1 := make(chan string)
+	output2 := make(chan string)
+
+	go server1(output1)
+	go server2(output2)
+
+	select {
+	case s1 := <-output1:
+		fmt.Println(s1)
+    case s2 := <-output2:
+		fmt.Println(s2)
+
+	default:
+		fmt.Println("No data available")
+	}
+}
