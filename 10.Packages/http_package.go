0c6930f59dc0b676ac14cdac4d542b9490688c67
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-28 00:22:59 +0600

    Packages

--- 10.Packages/http_package.go
+++ 10.Packages/http_package.go
@@ -12,5 +12,6 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", helloHandler)
 
+	fmt.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
