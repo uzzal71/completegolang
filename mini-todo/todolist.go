30213af90d68e8abde9a0e4485b3c90960b31f47
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-29 00:35:24 +0600

    todo

--- mini-todo/todolist.go
+++ mini-todo/todolist.go
@@ -7,11 +7,16 @@ type TodoList struct {
 }
 
 func (td *TodoList) AddTask(taskName string) {
-	fmt.Println("Task Added: ", taskName)
+	taskId := len(td.tasks) + 1
+	task := NewTask(taskId, taskName)
+	td.tasks = append(td.tasks, task)
+	fmt.Println("Task Added Successfully!")
 }
 
 func (td *TodoList) ViewTask() {
-	fmt.Println("Tasks are", td.tasks)
+	for i, task := range td.tasks {
+        fmt.Printf("[] %d: %v\n", i+1, task.Name)
+    }
 }
 
 func (td *TodoList) MarkTaskAsDone(taskId int) {
