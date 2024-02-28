7b830aac1c872879e4a1a633f11bc4f86ee89243
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-29 00:07:10 +0600

    todo

--- mini-todo/task.go
+++ mini-todo/task.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 type Task struct {
 	ID int
 	Name string
 	Done bool
+}
+
+func (t *Task) MaskAsDone() {
+	if t.Done {
+		fmt.Println("Task already done.")
+	}
+	t.Done = true
+}
+
+func NewTask(taskId int, taskName string) Task {
+	return Task{
+        ID: taskId,
+        Name: taskName,
+    }
 }
