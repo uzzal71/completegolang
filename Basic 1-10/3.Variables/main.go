329d15f379e6683d8b224352c2f1d97b00bf17a6
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-04 23:46:16 +0600

    variable

--- Basic 1-10/3.Variables/main.go
+++ Basic 1-10/3.Variables/main.go
@@ -7,8 +7,13 @@ func main() {
 	b := 5.5
 	c := true
 	result := a + 10
+
     fmt.Printf("A = %d\n", a)
 	fmt.Printf("B = %f\n", b)
 	fmt.Printf("C = %t\n", c)
 	fmt.Printf("Result = %d\n", result)
+
+	if c == true {
+		fmt.Println("C is true")
+	}
 }
