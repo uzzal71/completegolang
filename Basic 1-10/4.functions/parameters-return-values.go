8d5f5a80fa0e1157f0c00e9bb4bc3fce5e5567da
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-06 23:27:10 +0600

    functions

---
+++ Basic 1-10/4.functions/parameters-return-values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func calculate(x int, y int) (int, int) {
+	sum := x + y
+	diff := x - y
+	return sum, diff
+}
+
+func main() {
+	s, d := calculate(10, 5)
+	fmt.Println(s, d)
+}
