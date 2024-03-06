8a1a1d0e095a18db19073fb30810ad22d1edbca1
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-06 23:45:33 +0600

    functions

--- Basic 1-10/4.functions/pass-by-value-and-reference.go
+++ Basic 1-10/4.functions/pass-by-value-and-reference.go
@@ -2,19 +2,13 @@ package main
 
 import "fmt"
 
-type Person struct {
-	Name string
-	Age int
-	Email string
-	Phone string
-	Address string
-	Password string
-	OTP string
+func passByValue(x int) {
+	x = 10
 }
 
 func main() {
-	num1 := 5
-	num2 := 10
-	result := num1 + num2
-	fmt.Printf("%d\n", result)
+	// pass by value
+	value := 5
+	passByValue(value)
+    fmt.Println(value)
 }
