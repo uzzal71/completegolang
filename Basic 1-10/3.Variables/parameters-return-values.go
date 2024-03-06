5760122a3de1b7fd95de2786c719ea323a2e0926
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-06 23:22:43 +0600

    functions

--- Basic 1-10/3.Variables/parameters-return-values.go
+++ Basic 1-10/3.Variables/parameters-return-values.go
@@ -1,17 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	a := 5
-	b := 5.5
-	c := true
-	result := a + 10
-
-    fmt.Printf("A = %d\n", a)
-	fmt.Printf("B = %f\n", b)
-	fmt.Printf("C = %t\n", c)
-	fmt.Printf("Result = %d\n", result)
-
-	e := 5
+	
 }
