de24df4ac20fe2d751d86f911cb114d2b6a4bcc2
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-06 23:29:57 +0600

    functions

--- Basic 1-10/4.functions/parameters-return-values.go
+++ Basic 1-10/4.functions/parameters-return-values.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+/*
 func calculate(x int, y int) (int, int) {
 	sum := x + y
 	diff := x - y
 	return sum, diff
+}
+*/
+
+func calculate(x int, y int) (sum int, diff int) {
+	sum = x + y
+	diff = x - y
+	return
 }
 
 func main() {
