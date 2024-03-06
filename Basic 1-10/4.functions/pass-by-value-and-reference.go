3659efe2b15e2c81f05ea54cf92f9529fe637788
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-06 23:46:22 +0600

    functions

--- Basic 1-10/4.functions/pass-by-value-and-reference.go
+++ Basic 1-10/4.functions/pass-by-value-and-reference.go
@@ -6,9 +6,18 @@ func passByValue(x int) {
 	x = 10
 }
 
+func passByReference(x *int) {
+    *x = 10
+}
+
 func main() {
 	// pass by value
 	value := 5
 	passByValue(value)
     fmt.Println(value)
+
+	// pass by reference
+	pointer := 5
+    passByReference(&pointer)
+    fmt.Println(pointer)
 }
