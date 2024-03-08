78f11066959a9cd2e5bf2b7d9ad2239a70d60b84
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-08 21:21:17 +0600

    variables

--- Basic 1-10/4.functions/anonymous_function.go
+++ Basic 1-10/4.functions/anonymous_function.go
@@ -16,7 +16,8 @@ func main() {
 	result2 := sum(2, 3)
 	fmt.Println(result2)
 
-	result3 := func(x int, y int) (int, int) {
-        return x + y, x - y
+	result3 := func(x int, y int) int {
+        return x + y
     }(2, 3)
+	fmt.Println(result3)
 }
