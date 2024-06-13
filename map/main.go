8d26715013c071c24320e29fa6826b61a8944bbf
uzzal71 <uzzalroy.acm@gmail.com>
2024-06-14 02:15:15 +0600

    golang map

--- map/main.go
+++ map/main.go
@@ -5,14 +5,32 @@ import "fmt"
 func main() {
 	// var colors map[string]string
 
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
+	colors := make(map[int]string)
 
 	// colors := map[string]string{
 	// 	"red":   "#ff0000",
 	// 	"green": "#4bf745",
 	// }
 
-	colors["white"] = "#ffffff"
+	colors[10] = "#ffffff"
+
+	delete(colors, 10)
 
 	fmt.Println(colors)
+
+	colorAgains := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"black": "#000000",
+		"white": "#ffffff",
+	}
+
+	printMap(colorAgains)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
