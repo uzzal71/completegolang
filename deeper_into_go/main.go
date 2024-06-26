5ea4d538397385a0342a41a2602733235a121485
uzzal71 <uzzalroy.acm@gmail.com>
2024-06-26 19:42:01 +0600

    go approach

--- deeper_into_go/main.go
+++ deeper_into_go/main.go
@@ -1,14 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of DiamondS", newCard()}
+	cards := deck{"Ace of DiamondS", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
