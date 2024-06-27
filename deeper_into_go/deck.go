33772a30720e8104c3f90a4a0060a4e0d64bb235
uzzal71 <uzzalroy.acm@gmail.com>
2024-06-27 23:47:24 +0600

    go approach

--- deeper_into_go/deck.go
+++ deeper_into_go/deck.go
@@ -7,8 +7,18 @@ import "fmt"
 type deck []string
 
 func newDeck() deck {
-	cards := deck{"Ace of DiamondS", "Six of Spades"}
-	cards = append(cards, "Six of Spades")
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
 }
 
 func (d deck) print() {
